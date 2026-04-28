849a5f94fd1c32caaa84e0091ff61a239a07ccbb
Orion <danniliho@gmail.com>
2026-04-27 21:12:36 -0600

    login dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,23 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginDto } from './dto/login.dto';
+
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+  ) {}
 
   @Post('register')
-  async register(@Body() createUserDto: CreateUserDto) {
-    return await this.authService.register(createUserDto);
+  async register(@Body() registerUserDto: RegisterUserDto) {
+    return await this.authService.register(registerUserDto);
+  }
+
+  @Post('login')
+   async login(@Body() loginDto: LoginDto) {
+    // return 'Login successful';
+    return await this.authService.login(loginDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "src/users/entities/user.entity";
 import { Repository } from "typeorm";
-import { CreateUserDto } from "./dto/create-user.dto";
+import { RegisterUserDto } from "./dto/register-user.dto";
+import { LoginDto } from "./dto/login.dto";
+
 
 @Injectable()
 export class AuthService {
@@ -11,15 +13,22 @@ export class AuthService {
     private userRepository: Repository<User>,
   ) {}
 
-  async register(createUserDto: CreateUserDto) {
+  async register(registerUser: RegisterUserDto) {
     return this.userRepository.insert({
-      nombre:createUserDto.nombre,
-      apellido_paterno:createUserDto.apellido_paterno,
-      apellido_materno:createUserDto.apellido_materno,
-      email:createUserDto.email,
-      password:createUserDto.password,
-      telefono:createUserDto.telefono,
-      rol_id:2,
+      ...registerUser,
     });
+  }
+
+  async login(loginDto: LoginDto) {
+    // console.log('Login DTO:', loginDto); // Debugging line
+    const { email, password } = loginDto;
+    const user = await this.userRepository.findOne({ where: { email } }); 
+    if (!user) {
+      return { message: 'User not found' };
+    } 
+    if (user.password !== password) {
+      return { message: 'Invalid password' };
+    }
+    return { message: 'Login successful' };
   }
 }
