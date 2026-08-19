8385edb0ef23481feaa672c98e0070fce1895e2f
Orion <danniliho@gmail.com>
2026-08-18 22:34:19 -0600

    jwt seguimiento

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginDto } from './dto/login.dto';
-
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { GetUser } from './get-user.decorator';
+import { User } from 'src/users/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -16,8 +18,14 @@ export class AuthController {
   }
 
   @Post('login')
-   async login(@Body() loginDto: LoginDto) {
-    // return 'Login successful';
+  async login(@Body() loginDto: LoginDto) {
     return await this.authService.login(loginDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async me(@GetUser() user: User) {
+    const { password, ...safeUser } = user;
+    return safeUser;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,21 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from 'src/users/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController],
-  exports: [AuthService],
   imports: [
     TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'mi_secreto_super_seguro', // luego usa .env
+      signOptions: { expiresIn: '1d' },
+    }),
   ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
